cd3767008b42d24e6bc60061867777a6a1a49e02
Robert Skwiat <RSkwiat@gmail.com>
2015-11-22 17:53:09 -0500

    now with errors

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -5,10 +5,10 @@ $(function(){
     stories: 'stories.html',
     bookmarks: 'bookmarks.html',
     facebook: 'http://facebook.com',
-    twitter: 'http://twitter.com'
+    twitter: 'http://twitter.com',
   }
 
-  $('.search').on('keydown', function(event){
+  $('.search').on('key-Down', function(event){
     if ( event.which == 13 ) {
       console.log('enter was pressed');
       event.preventDefault();
